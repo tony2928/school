d6772e0e8eccd7bd13ac3bf8f03fe116b6a1a437
tony2928 <89720332+tony2928@users.noreply.github.com>
2023-06-21 09:51:20 -0600

    button quitar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let video = document.getElementsByClassName("video")[0];
+let quitar = document.getElementById("quitarVideo");
 let chekpoint = document.getElementsByClassName("video_checkpoint")[0];
 let chekpoint2 = document.getElementsByClassName("video_checkpoint")[1];
 let continuar = document.getElementById("continuar");
@@ -20,12 +21,14 @@ window.addEventListener("scroll", function() {
     if (isInViewport(chekpoint) || isInViewport(chekpoint2)) {
         video.classList.add("in_place");
         video.classList.remove("video_floating");
+        quitarVideo.style.display = "none";
         firstTime = false;
         buttonPressed = false
     } else {
         if (firstTime == false && buttonPressed == false) {
             video.classList.add("video_floating");
             video.classList.remove("in_place");
+            quitarVideo.style.display = "block";
         }
     }
 });
